a808a75d30af35be45f5a0301cd4c4372003a68d
Alex Kotov <kotovalexarian@gmail.com>
2020-10-05 12:36:08 +0500

    Add Puma

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ source 'https://rubygems.org'
 
 gem 'activesupport', '~> 6.0'
 gem 'bootsnap', '~> 1.4', require: false
+gem 'puma', '~> 5.0'
 gem 'rack', '~> 2.2'
 gem 'rack-attack', '~> 6.3'
 gem 'sinatra', '~> 2.1'
---
+++ config/puma/production.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+root_dir = File.expand_path('../..', __dir__).freeze
+
+workers_count     = ENV.fetch('WEB_CONCURRENCY') { 1 }
+max_threads_count = ENV.fetch('MAX_THREADS') { 5 }
+min_threads_count = ENV.fetch('MIN_THREADS') { max_threads_count }
+
+# Specifies the number of `workers` to boot in clustered mode.
+# Workers are forked webserver processes. If using threads and workers together
+# the concurrency of the application would be max `threads` * `workers`.
+# Workers do not work on JRuby or Windows (both of which do not support
+# processes).
+#
+workers workers_count
+
+# Puma can serve each request in a thread from an internal thread pool.
+# The `threads` method setting takes two numbers: a minimum and maximum.
+# Any libraries that use thread pools should be configured to match
+# the maximum value specified for Puma. Default is set to 5 threads for minimum
+# and maximum; this matches the default thread size of Active Record.
+#
+threads min_threads_count, max_threads_count
+
+# Use the `preload_app!` method when specifying a `workers` number.
+# This directive tells Puma to first boot the application and load code
+# before forking the application. This takes advantage of Copy On Write
+# process behavior so workers use less memory.
+#
+preload_app!
+
+# Bind the server to "url". "tcp://", "unix://" and "ssl://" are the only
+# accepted protocols.
+#
+bind "unix://#{File.join(root_dir, 'tmp', 'puma.sock')}"
+
+# Use "path" as the file to store the server info state. This is
+# used by "pumactl" to query and control the server.
+#
+state_path File.join(root_dir, 'tmp', 'puma.state')
