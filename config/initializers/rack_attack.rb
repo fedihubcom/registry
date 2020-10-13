fe5b4886276c44747b8e1534f290a3a16234105f
Alex Kotov <kotovalexarian@gmail.com>
2020-10-14 03:53:26 +0500

    Remove all code

--- config/initializers/rack_attack.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-# Be sure to restart your server when you modify this file.
-
-require 'active_support/cache'
-
-Rack::Attack.cache.store = ActiveSupport::Cache::MemoryStore.new
-
-Rack::Attack.throttle 'req/ip', limit: 120, period: 60, &:ip
