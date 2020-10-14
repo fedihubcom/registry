7fd495ed9f8ca013e4489c50cd4b8677603029fa
Alex Kotov <kotovalexarian@gmail.com>
2020-10-15 02:28:09 +0500

    Display users

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 #![feature(decl_macro, proc_macro_hygiene)]
 
+mod schema;
+
+#[macro_use] extern crate diesel;
 #[macro_use] extern crate rocket;
 #[macro_use] extern crate serde_derive;
 
-extern crate diesel;
 extern crate dotenv;
 extern crate rocket_contrib;
 
 use diesel::pg::PgConnection;
+use diesel::prelude::*;
 use diesel::r2d2::ConnectionManager;
 use r2d2::{Pool, PooledConnection};
 use rocket::{Outcome, Request, State};
@@ -44,7 +47,13 @@ impl Deref for DbConn {
 #[derive(Serialize)]
 struct TemplateContext {
     parent: &'static str,
-    users: Vec<&'static str>,
+    users: Vec<String>,
+}
+
+#[derive(Debug, Queryable)]
+struct User {
+    pub id: i32,
+    pub username: String,
 }
 
 fn main() {
@@ -54,12 +63,9 @@ fn main() {
 
 fn rocket() -> rocket::Rocket {
     rocket::ignite()
+        .manage(create_db_pool())
         .attach(Template::fairing())
         .mount("/", routes())
-}
-
-fn routes() -> Vec<rocket::Route> {
-    routes![index]
 }
 
 fn create_db_pool() -> Pool<ConnectionManager<PgConnection>> {
@@ -71,11 +77,20 @@ fn create_db_pool() -> Pool<ConnectionManager<PgConnection>> {
     Pool::new(manager).expect("Failed to create database pool")
 }
 
+fn routes() -> Vec<rocket::Route> {
+    routes![index]
+}
+
 #[get("/")]
-fn index() -> Template {
+fn index(db_conn: DbConn) -> Template {
+    use schema::users::dsl::*;
+
+    let all_users = users.load::<User>(&*db_conn).expect("Error loading users");
+    let all_user_names = all_users.iter().map(|user| user.username.to_string()).collect();
+
     let template_context = TemplateContext {
         parent: "layout",
-        users: vec!["foo", "bar", "car"],
+        users: all_user_names,
     };
 
     Template::render("index", &template_context)
