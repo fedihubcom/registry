f27b5e2d21c65d880b02bfd3030d45017a853a9c
Alex Kotov <kotovalexarian@gmail.com>
2020-10-15 04:38:55 +0500

    Create users

--- src/models.rs
+++ src/models.rs
@@ -1,6 +1,15 @@
+use super::schema::users;
+
 #[derive(Debug, Serialize, Queryable)]
 pub struct User {
     pub id: i32,
     pub username: String,
     pub encrypted_password: String,
+}
+
+#[derive(Debug, Insertable)]
+#[table_name="users"]
+pub struct NewUser<'a> {
+    pub username: &'a str,
+    pub encrypted_password: &'a str,
 }
--- src/routes.rs
+++ src/routes.rs
@@ -3,6 +3,7 @@ use crate::schema;
 use crate::models;
 
 use diesel::prelude::*;
+use rocket::response::Redirect;
 use rocket_contrib::templates::Template;
 
 #[derive(Serialize)]
@@ -12,7 +13,7 @@ struct TemplateContext {
 }
 
 pub fn routes() -> Vec<rocket::Route> {
-    routes![index]
+    routes![index, sign_up]
 }
 
 #[get("/")]
@@ -27,4 +28,23 @@ fn index(db_conn: database::DbConn) -> Template {
     };
 
     Template::render("index", &template_context)
+}
+
+#[post("/users/<username>/<password>")]
+fn sign_up(db_conn: database::DbConn, username: String, password: String) -> Redirect {
+    use schema::users;
+
+    let encrypted_password = bcrypt::hash(password, bcrypt::DEFAULT_COST).unwrap();
+
+    let new_user = models::NewUser {
+        username: username.as_str(),
+        encrypted_password: encrypted_password.as_str(),
+    };
+
+    diesel::insert_into(users::table)
+        .values(&new_user)
+        .get_result::<models::User>(&*db_conn)
+        .expect("Error creating user");
+
+    Redirect::to(uri!(index))
 }
