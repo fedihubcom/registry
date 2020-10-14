e86d1e33b7b9f339c30933a2bc6c4170279977fe
Alex Kotov <kotovalexarian@gmail.com>
2020-10-14 05:13:00 +0500

    Add layout

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,23 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
 #[macro_use] extern crate rocket;
+#[macro_use] extern crate serde_derive;
+extern crate rocket_contrib;
 
 use rocket_contrib::templates::Template;
 
+#[derive(Serialize)]
+struct TemplateContext {
+    parent: &'static str,
+}
+
 #[get("/")]
 fn index() -> Template {
-    Template::render("index", &1)
+    let template_context = TemplateContext {
+        parent: "layout",
+    };
+
+    Template::render("index", &template_context)
 }
 
 fn main() {
