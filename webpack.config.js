df38bedc378c7f2f59f6063fb5c85acb147ce065
Alex Kotov <kotovalexarian@gmail.com>
2020-10-16 01:39:01 +0500

    Build CSS bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,30 @@
 const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
-  entry: './src/assets/index.js',
+  entry: [
+    './src/assets/index.js',
+    './src/assets/index.css',
+  ],
   output: {
     path: path.resolve(__dirname, 'public/assets'),
     publicPath: '/assets/',
     filename: 'site.js',
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'site.css',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+        ],
+      },
+    ],
   },
 }
