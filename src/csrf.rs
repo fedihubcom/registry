5a6c15722fa8addcca3c36611d9c4f8aa272a2d2
Alex Kotov <kotovalexarian@gmail.com>
2020-10-16 12:36:55 +0500

    Extract CSRF token from session

--- src/csrf.rs
+++ src/csrf.rs
@@ -1,12 +1,15 @@
 use rand::RngCore;
 use rocket::{Data, Request};
 use rocket::fairing::{Fairing as RocketFairing, Info, Kind};
-use rocket::http::Cookie;
+use rocket::http::{Cookie, Status};
+use rocket::request::{FromRequest, Outcome};
 
 const COOKIE_NAME: &str = "csrf_token";
 const RAW_TOKEN_LENGTH: usize = 32;
 
 pub struct Fairing;
+
+pub struct Guard(Vec<u8>);
 
 impl Fairing {
     pub fn new() -> Self {
@@ -23,7 +26,7 @@ impl RocketFairing for Fairing {
     }
 
     fn on_request(&self, request: &mut Request, _: &Data) {
-        if request.cookies().get_private(COOKIE_NAME).is_some() { return };
+        if let Some(_) = valid_raw_from_request(request) { return }
 
         let mut raw = [0u8; RAW_TOKEN_LENGTH];
         rand::thread_rng().fill_bytes(&mut raw);
@@ -32,4 +35,23 @@ impl RocketFairing for Fairing {
 
         request.cookies().add_private(Cookie::new(COOKIE_NAME, encoded));
     }
+}
+
+impl<'a, 'r> FromRequest<'a, 'r> for Guard {
+    type Error = ();
+
+    fn from_request(request: &'a Request<'r>) -> Outcome<Self, Self::Error> {
+        match valid_raw_from_request(request) {
+            None => Outcome::Failure((Status::Forbidden, ())),
+            Some(old_raw) => Outcome::Success(Self(old_raw)),
+        }
+    }
+}
+
+fn valid_raw_from_request(request: &Request) -> Option<Vec<u8>> {
+    request.cookies().get_private(COOKIE_NAME)
+        .and_then(|cookie| base64::decode(cookie.value()).ok())
+        .and_then(|raw|
+            if raw.len() >= RAW_TOKEN_LENGTH { Some(raw) } else { None }
+        )
 }
