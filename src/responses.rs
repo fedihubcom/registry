33a7a6803a0d1cf6797db371cb41369009b4ad2b
Alex Kotov <kotovalexarian@gmail.com>
2020-10-21 15:13:12 +0500

    Handle translation errors

--- src/responses.rs
+++ src/responses.rs
@@ -1,3 +1,4 @@
+use crate::i18n::I18nError;
 use crate::views;
 
 use rocket::response::Redirect;
@@ -6,6 +7,7 @@ use rocket_contrib::templates::Template;
 #[derive(Debug, rocket::response::Responder)]
 #[response(content_type = "text/html")]
 pub enum CommonResponse {
+    TranslationError(Redirect),
     AlreadySignedIn(Redirect),
     InvalidAuthenticityToken(Redirect),
     NotSignedIn(Redirect),
@@ -15,6 +17,12 @@ pub enum CommonResponse {
     InvalidForm(Template),
     #[response(status = 500)]
     UnknownError(Template),
+}
+
+impl From<I18nError> for CommonResponse {
+    fn from(_: I18nError) -> Self {
+        Self::TranslationError(Redirect::to("/"))
+    }
 }
 
 impl From<rocket_csrf::VerificationFailure> for CommonResponse {
--- src/routes/home.rs
+++ src/routes/home.rs
@@ -14,11 +14,11 @@ pub fn index(
     csrf_token: CsrfToken,
     current_user: states::MaybeCurrentUser,
 ) -> Result<Template, CommonResponse> {
-    let l10n = i18n.l10n("en").unwrap();
+    let l10n = i18n.l10n("en")?;
 
     let page_context = views::home::Index {
-        i18n_fedihub: l10n.translate("fedihub").unwrap(),
-        i18n_federated_services_without_censorship: l10n.translate("federated-services-without-censorship").unwrap(),
+        i18n_fedihub: l10n.translate("fedihub")?,
+        i18n_federated_services_without_censorship: l10n.translate("federated-services-without-censorship")?,
     };
 
     let context = views::Site {
