898d7da50b61bddc0a942e1380db1e78a190c9e1
Alex Kotov <kotovalexarian@gmail.com>
2020-10-21 10:54:10 +0500

    Add associations

--- src/models/employee.rs
+++ src/models/employee.rs
@@ -1,12 +1,32 @@
 use crate::database::DbConn;
 
 use crate::schema::employees;
+use crate::schema::employee_contacts;
+use crate::schema::employee_infos;
 
 use diesel::prelude::*;
 
-#[derive(Debug, Serialize, Queryable)]
+#[derive(Debug, Identifiable, Serialize, Queryable)]
 pub struct Employee {
     pub id: i32,
+    pub name: String,
+    pub role: String,
+}
+
+#[derive(Associations, Debug, Identifiable, Serialize, Queryable)]
+#[belongs_to(Employee)]
+pub struct EmployeeContact {
+    pub id: i32,
+    pub employee_id: i32,
+    pub name: String,
+    pub link: String,
+}
+
+#[derive(Associations, Debug, Identifiable, Serialize, Queryable)]
+#[belongs_to(Employee)]
+pub struct EmployeeInfo {
+    pub id: i32,
+    pub employee_id: i32,
     pub name: String,
     pub role: String,
 }
